c3214546c69630191f4a96e91abf612585f5703e
Ronyx <ronnie94official@gmail.com>
2019-05-27 14:50:07 +0530

    secured token

---
+++ client_v1.1.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "?"
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+      print("Powering up the bot")
+
+@client.event
+async def on_message(message):
+      author = message.author
+      content = message.content
+      print("{} texted: {}".format(author, content))
+
+@client.event
+async def on_message_delete(message):
+      author = message.author
+      content = message.content
+      channel = message.channel
+      await channel.send('{} deleted this thread: {}'.format(author, content))
+
+
+client.run(TOKEN)
