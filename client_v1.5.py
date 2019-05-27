95a0ad5b9bb9e256b14cd5768030a09a8499e379
Ronyx <ronnie94official@gmail.com>
2019-05-27 21:34:56 +0530

    delete messages from channel

---
+++ client_v1.5.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+import datetime as dt
+from datetime import datetime
+
+TOKEN = "?"
+
+#setting date configurations
+delta = dt.timedelta(days=1)
+last = datetime.now() - delta
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+      print("Powering up the bot")
+
+
+@client.command(pass_context=True)
+async def clear(ctx, amount=100,):
+      await ctx.channel.purge()
+      await ctx.channel.send('Messages deleted!')
+      
+      
+client.run(TOKEN)
