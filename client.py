e39775e5512a9bb6cde2625c1f629f0599a84855
Ronyx <ronnie94official@gmail.com>
2019-05-27 14:05:34 +0530

    bot responses on channel

--- client.py
+++ client.py
@@ -15,5 +15,12 @@ async def on_message(message):
       content = message.content
       print("{} texted: {}".format(author, content))
 
+@client.event
+async def on_message_delete(message):
+      author = message.author
+      content = message.content
+      channel = message.channel
+      await channel.send('{} deleted this thread: {}'.format(author, content))
+
 
 client.run(TOKEN)
