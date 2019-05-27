de3ef4b8eddd6dff2f423771995c20d389c6d5b0
Ronyx <ronnie94official@gmail.com>
2019-05-27 15:42:12 +0530

    event handling and command processing

---
+++ client_v1.3.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "?"
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+      print("Powering up the bot")
+
+@client.event
+async def on_message(message):
+      print("A new message!")
+      await client.process_commands(message)
+
+@client.command()
+async def ping(ctx):
+      await ctx.channel.send("Pong!")
+      
+
+@client.command()
+async def echo(ctx, *args):
+      output = ""
+      for words in args:
+            output += words
+            output += " "
+      await ctx.channel.send(output)
+
+client.run(TOKEN)
+           
+client.run(TOKEN)
