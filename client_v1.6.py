5feb740db6c6badcfc86ce04c4a0a827abd1e621
Ronyx <ronnie94official@gmail.com>
2019-05-28 14:12:48 +0530

    handling background events - updated

--- client_v1.6.py
+++ client_v1.6.py
@@ -6,16 +6,32 @@ from itertools import cycle
 TOKEN = "?"
 
 client = commands.Bot(command_prefix = '.')
-status = ['Resting','Crawling']
+status = ['Rest','Crawl']
+
+async def change_status():
+      await client.wait_until_ready()
+      msgs = cycle(status)
+
+      while not client.is_closed:
+            current_status = next(msgs)
+            await client.change_presence(activity=discord.Game(name=current_status))
+            await asyncio.sleep(5)
 
 @client.event
 async def on_ready():
+      #await client.change_presence(activity=discord.Game(name="Live"))
       print("Powering up the bot")
-
+      print('\nBot_Name: {}\nBot_ID: {}\nConnected Servers: {}\n'.format(
+        client.user.name,  #Bot Name
+        client.user.id,         #Bot ID
+        len(client.guilds))  #Number of servers currently connected to
+    )
+      
 
 @client.command()
 async def logout(ctx):
       await client.logout()
       
-      
+
+client.loop.create_task(change_status()) 
 client.run(TOKEN)
