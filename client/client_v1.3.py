fb964b858b44eeb0a9b6ac99f0c8192ce0db588e
Ronyx <ronnie94official@gmail.com>
2019-05-29 21:47:39 +0530

    event handling and using discords' command processing

--- client/client_v1.3.py
+++ client/client_v1.3.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 
+'''
+event handling and using discords' command processing
+'''
+
+
 TOKEN = "?"
 
 client = commands.Bot(command_prefix = '.')
@@ -17,7 +22,6 @@ async def on_message(message):
 @client.command()
 async def ping(ctx):
       await ctx.channel.send("Pong!")
-      
 
 @client.command()
 async def echo(ctx, *args):
