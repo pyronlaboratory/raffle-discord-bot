e651654aa420a35c6ffa4785e7f3e0009e67f1db
Ronyx <ronnie94official@gmail.com>
2019-06-03 19:52:02 +0530

    updated story 1.5

--- raffle_server/raffle_form/views.py
+++ raffle_server/raffle_form/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello Fellaz!')
+	return render(request, 'raffle_form/raffleform.html')
